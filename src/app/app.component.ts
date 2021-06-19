0633f4f3fd3f44e8659a86099ccd802cf1082f9c
schenzio <matteo.scanna1999@gmail.com>
2021-06-19 12:49:52 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,11 @@ import {
   transition,
 } from '@angular/animations';
 
+export class chapter {
+  title: string;
+  weight: boolean
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -29,23 +34,36 @@ import {
   ]
 
 })
+
 export class AppComponent implements OnInit {
   title: string = 'la povertà sanitaria';
   exp: string = 'clicca sulle parti del logo';
   logoIndex: HTMLCollection = document.getElementsByClassName('chap');
-  index: Array<string> = ['intro', 'sezione1', 'sezione2', 'sezione3'];
-
+  //index: Array<string> = ['intro', 'sezione1', 'sezione2', 'sezione3'];
+  index: Array<chapter> = [
+    {title:'intro', weight: false}, 
+    {title: 'sezione1', weight: false}, 
+    {title: 'sezione2', weight: false}, 
+    {title: 'sezione3', weight: false}
+  ];
   selection: string = undefined;
   subtitle: string = "";
   /*isOpen = true;
   toggle() {
     this.isOpen = !this.isOpen;
   }*/
-  change(node: any, color: string) {
-    node.style.fill = color;
-    let section = document.getElementById('nav').children[node.id];
+  focus(id) {
+    this.index[id].weight=true;
+    document.getElementById(id).style.fill = '#eb836a';
     }
-
+  defocus(id){
+      this.index[id].weight=false;
+      if(id == '3'){
+        document.getElementById(id).style.fill = 'white';
+      } else {
+        document.getElementById(id).style.fill = '#d7232a';
+      }
+  }
   showChapter(n: string){
     this.selection = n;
     this.subtitle = this.index[n];
@@ -56,16 +74,17 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
+    document.getElementById('3').style.fill = 'white';
     for (let i = 0; i < this.logoIndex.length; i++) {
       let elem = this.logoIndex[i];
       elem.addEventListener(
         'mouseover',
-        () => this.change(elem, '#eb836a'),
+        () => this.focus(elem.id),
         false
       );
       elem.addEventListener(
         'mouseout',
-        () => this.change(elem, '#d7232a'),
+        () => this.defocus(elem.id),
         false
       );
       elem.addEventListener('click', () => this.showChapter(elem.id), false);
